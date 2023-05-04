2efb5b708b8b590af5bd3586ed75915d39b25028
marcosgSI <34721745+marcosgSI@users.noreply.github.com>
2023-05-04 16:53:34 -0300

    Update chat.js

--- templates/chat.js
+++ templates/chat.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Conectar ao servidor SocketIO
-const socket = io.connect('http://localhost:8000');
-        });
+const socket = io.connect('https://barbosahoff.herokuapp.com');
+
 
   // Enviar mensagem ao servidor ao enviar o formulário
   document.querySelector('#message-form').onsubmit = (e) => {
